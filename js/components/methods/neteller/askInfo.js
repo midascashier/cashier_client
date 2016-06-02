99e9567a7512c0afa246dca9bbe706f8809ebd01
jocampo <jocampoz@gmail.com>
2016-06-02 10:57:24 -0600

    getInitialState

--- js/components/methods/neteller/askInfo.js
+++ js/components/methods/neteller/askInfo.js
@@ -3,14 +3,20 @@ import {InfoMethod} from './infoMethod'
 import {CashierStore} from '../../../stores/CashierStore';
 
 const AskInfo = React.createClass({
-	render() {
+	getInitialState: function () {
 		CashierStore.setCurrentStep("askInfo");
+		return null;
+	},
+	render() {
 		return (
 			<div id="methods">
 				<h3>Neteller Ask Info</h3>
-				Neteller Account: <input type="text" /><hr/><br/>
-				Enabled 2FA: <input type="text" /><br/><hr />
-				AMOUNT: <input type="text" /><br /><br/><br/><br/>
+				Neteller Account: <input type="text"/>
+				<hr/>
+				<br/>
+				Enabled 2FA: <input type="text"/><br/>
+				<hr />
+				AMOUNT: <input type="text"/><br /><br/><br/><br/>
 			</div>
 		)
 	}
--- js/components/methods/neteller/infoMethod.js
+++ js/components/methods/neteller/infoMethod.js
@@ -1,38 +1,47 @@
 import React from 'react'
-import { Link } from 'react-router'
+import {Link} from 'react-router'
 import {MethodList} from '../../contentComponents/methodsList'
 import {CashierStore} from '../../../stores/CashierStore';
 
 const InfoMethod = React.createClass({
-	render() {
-		let button_text="";
-		let next_step="";
-		if (CashierStore.getCurrentStep()=="infoMethod"){
-			button_text="Deposit With Neteller";
-			next_step="askinfo";
-		}else{
-			button_text="Complete Deposit";
-			next_step="ticket";
+	getInitialState: function () {
+		let button_text = "";
+		let next_step = "";
+		if (CashierStore.getCurrentStep() == "infoMethod") {
+			button_text = "Deposit With Neteller";
+			next_step = "askinfo";
+		} else {
+			button_text = "Complete Deposit";
+			next_step = "ticket";
 		}
+		return { button_text: button_text, next_step: next_step};
+	},
+	render() {
 		return (
 			<div id="infoMethod">
 				<h3>Neteller Deposit Limits</h3>
-				Min.Deposit $10<hr/><br/>
-				Max. Deposit $640 / 24 Hours<br/><hr />
+				Min.Deposit $10
+				<hr/>
+				<br/>
+				Max. Deposit $640 / 24 Hours<br/>
+				<hr />
 				Remaining Limit: $640<br /><br/><br/><br/>
-				<b><Link to={`/deposit/neteller/${next_step}`}>{button_text} >></Link></b>
+				<b><Link to={`/deposit/neteller/${this.state.next_step}`}>{this.state.button_text} >></Link></b>
 			</div>
 		)
 	}
 });
 
 let NetellerInfo = React.createClass({
-	render() {
+	getInitialState: function () {
 		CashierStore.setCurrentStep("infoMethod");
+		return null;
+	},
+	render() {
 		return (
 			<div>
-			<MethodList />
-			<InfoMethod />
+				<MethodList />
+				<InfoMethod />
 			</div>
 		)
 	}
