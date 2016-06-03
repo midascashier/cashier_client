9a5a033f26f626dee50377235308d6f47facbb81
jocampo <jocampoz@gmail.com>
2016-06-03 11:35:42 -0600

    validator

---
+++ js/components/MyInput.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const MyInput = React.createClass({
+	mixins: [Formsy.Mixin],
+	changeValue(event) {
+		this.setValue(event.currentTarget[this.props.type === 'checkbox' ? 'checked' : 'value']);
+	},
+	render() {
+		const className = 'form-group' + (this.props.className || ' ') + (this.showRequired() ? 'required' : this.showError() ? 'error' : null);
+		const errorMessage = this.getErrorMessage();
+		return (
+			<div className={className}>
+				<label htmlFor={this.props.name}>{this.props.title}</label>
+				<input
+					type={this.props.type || 'text'}
+					name={this.props.name}
+					onChange={this.changeValue}
+					value={this.getValue()}
+					step={this.props.step === 'any' ? 'any' : null}
+					checked={this.props.type === 'checkbox' && this.getValue() ? 'checked' : null}
+				/>
+				<span className='validation-error'>{errorMessage}</span>
+			</div>
+		);
+	}
+});
+
+module.exports.MyInput = MyInput;
--- js/components/methods/neteller/askInfo.js
+++ js/components/methods/neteller/askInfo.js
@@ -1,22 +1,32 @@
 import React from 'react'
 import {InfoMethod} from './infoMethod'
 import {CashierStore} from '../../../stores/CashierStore'
+import Formsy from 'formsy-react'
+import {MyInput} from '../../MyInput'
+
 
 const AskInfo = React.createClass({
 	getInitialState: function () {
 		CashierStore.setCurrentStep("askInfo");
-		return null;
+		return { canSubmit: false };
+	},
+	submit(data) {
+		alert(JSON.stringify(data, null, 4));
+	},
+	enableButton() {
+		this.setState({ canSubmit: true });
+	},
+	disableButton() {
+		this.setState({ canSubmit: false });
 	},
 	render() {
 		return (
 			<div id="methods">
 				<h3>Neteller Ask Info</h3>
-				Neteller Account: <input type="text"/>
-				<hr/>
-				<br/>
-				Enabled 2FA: <input type="text"/><br/>
-				<hr />
-				AMOUNT: <input type="text"/><br /><br/><br/><br/>
+				<Formsy.Form onSubmit={this.submit} onValid={this.enableButton} onInvalid={this.disableButton} className="login">
+					<MyInput name="email" title="Neteller Account: " validations="isEmail" validationError="This is not a valid email" required />
+					<MyInput name="amount" title="Amount: " type="number" step="any" validations="isNumeric" validationError="This is not a valid amount" required />
+				</Formsy.Form>
 			</div>
 		)
 	}
