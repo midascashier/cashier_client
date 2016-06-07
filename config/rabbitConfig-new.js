f3fcd6a4d958afe899345182ba5f6fe7896dab7c
joshgabry <josua.obando@gmail.com>
2016-06-07 17:07:16 -0600

    Add rabbitConfig-new

---
+++ config/rabbitConfig-new.js
@@ -0,0 +1,9 @@
+/**
+ * Rabbit Credentials
+ */
+export let rabbitConfig = new Map()
+	.set("ip","10.0.223.142")
+	.set("port","15674")
+	.set("user","midas")
+	.set("pass","midas")
+	.set("virtual","vh");
