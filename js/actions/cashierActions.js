087cf96251d05378b1bf94a2b569eba30e0209fb
joshgabry <josua.obando@gmail.com>
2016-06-10 15:12:53 -0600

    action processors

--- js/actions/cashierActions.js
+++ js/actions/cashierActions.js
@@ -29,17 +29,15 @@ let CashierActions = {
       action: actions.PROCESSORS_RESPONSE,
       data: data
     });
+  },
+
+  countries_response: function(data){
+    CashierDispatcher.handleServerAction({
+      action: actions.COUNTRIES_RESPONSE,
+      data: data
+    });
   }
 
-	},
-
-	countries_response: function(data){
-		CashierDispatcher.handleServerAction({
-			action: actions.COUNTRIES_RESPONSE,
-			data: data
-		});
-	}
-	
 };
 
 module.exports.CashierActions = CashierActions;
