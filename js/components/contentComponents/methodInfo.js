fc4988b0d4a4fb23c55f5f1a4fcde337e28e87ca
jocampo <jocampoz@gmail.com>
2016-06-16 08:58:50 -0600

    proptypes en methodInfo

--- js/components/contentComponents/methodInfo.js
+++ js/components/contentComponents/methodInfo.js
@@ -1,10 +1,23 @@
 import React from 'react'
+import {Loading} from '../loading'
 
 let MethodInfo = React.createClass({
+
+	propTypes: {
+		selectedProcessorName: React.PropTypes.string,
+		customerAction: React.PropTypes.string
+	},
+
 	render() {
 		return (
 			<div id="methodInfo">
-				METHOD
+				{(() => {
+					if (!this.props.selectedProcessorName) {
+						return <Loading />;
+					}else{
+						return this.props.selectedProcessorName + ' ' + this.props.customerAction + ' Limits'
+					}
+				})()}
 			</div>
 		)
 	}
