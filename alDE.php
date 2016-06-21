70e40f042ac01f86396e577ac740ea114cead9bd
joshgabry <josua.obando@gmail.com>
2016-06-21 10:24:30 -0600

    add ACUITYTEC

--- alDE.php
+++ alDE.php
@@ -12,39 +12,60 @@
   <input type="hidden" id="ioBB" name="ioBB" value="">
   <input type="hidden" id="atDeviceId" name="atDeviceId" value="">
 </form>
-<!-- scripts -->
-<script>
-  //var io_install_flash = false;
-  var io_install_stm = false;
-  var io_exclude_stm = 12;
-  // Network Latency
-  var io_submit_element_id = 'btnlogin'; // Forms Submit Button
-  var io_max_wait = 30; // If BB is not ready after this, submit
-  var io_submit_form_id = 'alForm';
-  var ioCompleted = false;
+    <!-- scripts -->
+    <!-- IOVATION -->
+    <script src="https://mpsnare.iesnare.com/snare.js"></script>
+    <!-- ACUITYTEC -->
+    <script src="https://service1.acuitytec.com/detect/detect3.min.js"></script>
+    <script>
 
-  //get the blackBox
-  var io_bb_callback = function(bb, isComplete){
-    ioCompleted = isComplete;
-    if(isComplete){
-      // Element to hold the BlackBox
-      var hidden_field = document.getElementById('ioBB');
-      if(hidden_field){
-        hidden_field.value = bb;
-      }
-    }
-  };
+        // IOVATION
+        var io_install_stm = false;
+        var io_exclude_stm = 12;
+        // Network Latency
+        var io_submit_element_id = 'btnlogin'; // Forms Submit Button
+        var io_max_wait = 30; // If BB is not ready after this, submit
+        var io_submit_form_id = 'alForm';
+        var ioCompleted = false, atCompleted = false;
 
-  var checkTimeOut = setInterval(function(){
-    if(ioCompleted){
-      clearInterval(checkTimeOut);
-      var login_form = document.getElementById('alForm');
-      if (login_form){
-        login_form.submit();
-      }
-    }
-  }, 100);
-</script>
-<script src="https://mpsnare.iesnare.com/snare.js"></script>
+        //get the blackBox
+        var io_bb_callback = function(bb, isComplete){
+            ioCompleted = isComplete;
+            if(isComplete){
+              // Element to hold the BlackBox
+              var hidden_field = document.getElementById('ioBB');
+              if(hidden_field){
+                hidden_field.value = bb;
+              }
+            }
+        };
+
+        // ACUITYTEC
+        var at_max_wait = 30;
+        function receiveData(data){
+            var deviceId_field = document.getElementById('atDeviceId');
+            if(deviceId_field){
+                deviceId_field.value = data;
+            }
+            atCompleted = true;
+        }
+        requestData();
+
+        // Get Ids
+        var dateATInit = new Date();
+        var checkTimeOut = setInterval(function(){
+            var now = new Date();
+            var time = now.getTime() - dateATInit.getTime();
+
+            if((ioCompleted) && (atCompleted || time >= at_max_wait)){
+                clearInterval(checkTimeOut);
+                var login_form = document.getElementById('alForm');
+                if(login_form){
+                    login_form.submit();
+                }
+            }
+        }, 100);
+
+    </script>
 </body>
 </html>
