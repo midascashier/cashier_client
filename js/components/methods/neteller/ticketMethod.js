5430dd2f3708beb14eae78c3201dd7b00c3f9566
jocampo <jocampoz@gmail.com>
2016-06-23 10:43:46 -0600

    netneller confirm

--- js/components/methods/neteller/ticketMethod.js
+++ js/components/methods/neteller/ticketMethod.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import {Link} from 'react-router'
 
 let NetellerTicket = React.createClass({
 	render() {
 		return (
 			<div id="methods">
+				<Link to="/deposit/">test</Link>
 				<h1>Your $ deposit was successful.</h1><br />
 			</div>
 		)
