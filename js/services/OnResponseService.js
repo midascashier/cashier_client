552fc03880f5a758a8bc06f51aa388fe2eed82ef
jocampo <jocampoz@gmail.com>
2016-07-06 10:06:59 -0600

    onResponse change

--- js/services/OnResponseService.js
+++ js/services/OnResponseService.js
@@ -8,66 +8,29 @@ import actions from '../constants/Actions'
  */
 class OnResponseService {
 	processResponse(action, data){
-			switch(action){
-				case actions.LOGIN_RESPONSE:
-					data.application = loginInfo;
-					CashierActions.login_response(data);
-					customerService.customerLoginResponse();
-					break;
 
-				case actions.CUSTOMER_INFO_RESPONSE:
-					CashierActions.customerInfo_response(data);
-					applicationService.getStates();
-					break;
+		if(action == actions.LOGIN_RESPONSE){
+			data.application = loginInfo;
+		}
 
-				case actions.COMPANY_INFO_RESPONSE:
-					CashierActions.companyInfo_response(data);
-					break;
+		CashierActions.responses(action, data);
 
-				case actions.CUSTOMER_TRANSACTIONS_RESPONSE:
-					CashierActions.getCustomerTransactions_response(data);
-					break;
+		switch(action){
+			case actions.LOGIN_RESPONSE:
+				customerService.customerLoginResponse();
+				break;
 
-				case actions.COUNTRIES_RESPONSE:
-					CashierActions.countries_response(data);
-					break;
+			case actions.CUSTOMER_INFO_RESPONSE:
+				applicationService.getStates();
+				break;
 
-				case actions.STATES_RESPONSE:
-					CashierActions.states_response(data);
-					break;
+			case actions.PROCESSORS_RESPONSE:
+				customerService.CustomerProcessorsResponse(data);
+				break;
 
-				case actions.PROCESSORS_RESPONSE:
-					CashierActions.getCustomerProcessors_response(data);
-					customerService.CustomerProcessorsResponse(data);
-					break;
-
-				case actions.PAYACCOUNTS_BY_PROCESSOR_RESPONSE:
-					CashierActions.getCustomerPreviousPayAccount_response(data);
-					break;
-
-				case actions.PAYACCOUNTS_DISABLE_RESPONSE:
-					CashierActions.getDisablePayAccount_response(data);
-					break;
-
-				case actions.PROCESSORS_LIMIT_RULES_RESPONSE:
-					CashierActions.getProcessorLimitRules_response(data);
-					break;
-
-				case actions.PROCESSORS_LIMIT_MIN_MAX_RESPONSE:
-					CashierActions.getProcessorMinMaxLimits_response(data);
-					break;
-
-				case actions.PROCESS_RESPONSE:
-					if(data.response || data.state){
-						CashierActions.processResponse(data);
-					} else{
-						console.log(data);
-					}
-					break;
-
-				default:
-					console.log(data.state);
-			}
+			default:
+				console.log(data.state);
+		}
 	};
 }
 
