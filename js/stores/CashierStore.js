c103184694579d36493c1276afd9d2a6b2e38454
jocampo <jocampoz@gmail.com>
2016-07-07 09:57:01 -0600

    comments

--- js/stores/CashierStore.js
+++ js/stores/CashierStore.js
@@ -364,7 +364,6 @@ let _transactionResponse = {
 	},
 	bitCoinTransaction: { Address: null },
 	load(response){
-
 		// transaction data
 		let transaction = response.transaction;
 		this.transaction.journalId = transaction.journalId;
@@ -652,7 +651,7 @@ CashierStore.dispatchToken = CashierDispatcher.register((payload) =>{
 			break;
 
 		case actions.PAYACCOUNTS_DISABLE_RESPONSE:
-			let currentPayAccountId = CashierStore.getUI().payAccountId;
+			let currentPayAccountId = _UI().payAccountId;
 			if(currentPayAccountId){
 				_payAccounts.splice(currentPayAccountId, 1);
 			}
