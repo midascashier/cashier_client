da2752acf7a8be285c8fe839b1730ea3acccc180
jocampo <jocampoz@gmail.com>
2016-07-11 10:02:36 -0600

    header in progress

--- js/components/contentComponents/ProcessorInfo.js
+++ js/components/contentComponents/ProcessorInfo.js
@@ -63,7 +63,7 @@ let ProcessorInfo = React.createClass({
 
 		return (
 			<div id="infoLimits">
-				<p><a href="#">Good news! You have a <span>100%</span> deposit bonus up to <span>$1,000.</span></a></p>
+				<p>Good news! You have a <span>100%</span> deposit bonus up to <span>$1,000.</span></p>
 				<div className="row">
 					<div className="col-sm-12">
 						<div className="deposit-limits">
