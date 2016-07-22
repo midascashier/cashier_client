9576a177370ec22d4b646bfad455471b520e784b
jocampo <jocampoz@gmail.com>
2016-07-21 18:21:21 -0600

    form validation

--- js/constants/Application.js
+++ js/constants/Application.js
@@ -13,7 +13,7 @@ export let isValidate = (value, type) =>{
 	let regExp = [];
 	regExp['isCreditNumber'] = { Visa: /^4[0-9]{12}(?:[0-9]{3})?$/, MC: /^5[1-5][0-9]{14}$/ };
 	regExp['isCVV'] = { cvv: /^[0-9]{3}$/i };
-	regExp['isnumber']= { int: /^[0-9]+$/, float: /[0-9]+[,\.][0-9]+/ };
+	regExp['isNumber']= { int: /^[0-9]+$/, float: /[0-9]+[,\.][0-9]+/ };
 	regExp['isIP'] = { ip: /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/ };
 	regExp['isBitCoinAddress'] = { bitcoinAddress: /^[13][a-km-zA-HJ-NP-Z0-9]{26,33}$/i };
 	regExp['isEmail'] = { email: /^[_a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/ };
