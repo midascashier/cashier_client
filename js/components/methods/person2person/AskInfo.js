cfff17a080cc6057de46788237c2bc81afaca771
joshgabry <josua.obando@gmail.com>
2016-08-03 10:57:43 -0600

    update

--- js/components/methods/person2person/AskInfo.js
+++ js/components/methods/person2person/AskInfo.js
@@ -37,119 +37,105 @@ let AskInfo = React.createClass({
 		let displayName = UIService.getProcessorDisplayName();
 		let processingTitle = translate('PROCESSING_DEPOSIT_INFORMATION_TITLE_P2P', "Please Enter the Sender's Information");
 		let selectType = (!isWithDraw) ? translate('P2P_SELECT_DEPOSIT') : translate('P2P_SELECT_WITHDRAW');
+		let deleteButton = (!isWithDraw) ? translate('PROCESSING_BUTTON_DELETE_RECEIVER') : translate('PROCESSING_BUTTON_DELETE_SENDER');
 
 		return (
-			<div id="askInfoP2P" className="box">
+			<div id="p2pAskInfo" className="box">
 				<div className="row">
 					<div className="col-sm-12">
-						<div className="row">
-							<div className="col-sm-12">
-								<div className="title">{processingTitle}</div>
-							</div>
-							<div className="col-sm-12">
-								<div className="infoCol">
-									<div className="col-sm-12">
-										<div className="row">
-											<div className="col-sm-3">
-												<div className="method active pull-left">
-													<img className="img-responsive" src={originPath + '/images/processors/16.png'}
-															 title={displayName}/>
-												</div>
-											</div>
-											<div className="col-sm-9">
-												<div>
-													<div className="form-group" id="payAccount">
-														<label for="">{selectType}:</label>
-														{(() =>{
-															if(payAccountId != 0){
-																return (
-																	<div>
-																		<div className="col-sm-9">
-																			<SelectPayAccount setAmount={setAmount} amount={amount} />
-																		</div>
-																		<div className="col-sm-3">
-																			<button type='button' onClick={this.disablePayAccount} className='btn btn-xs btn-green'>
-																				{translate('PROCESSING_BUTTON_DELETE_ACCOUNT', 'Delete Account')}
-																			</button>
-																		</div>
-																	</div>
-																)
-															} else{
-																return (
-																	<div>
-																		<SelectPayAccount setAmount={setAmount} amount={amount} />
-																	</div>
-																)
-															}
-														})()}
-													</div>
-													<div className="form-group">
-														{(() =>{
-															if(payAccountId != 0 && !isWithDraw){
-																return (
-																	<div id="timeFrame">
-																		<label
-																			className="control-label">{translate('P2P_TIME_FRAME', 'What time will you send these funds?')}</label>
-																		<div className="col-sm-6">
-																			<div className="form-group">
-																				<select className="form-control"
-																								value={this.props.timeFrameDay}
-																								onChange={this.props.timeFrameDayChange}>
-																					<option value="TODAY">{translate('P2P_TIME_FRAME_TODAY', 'Today')}</option>
-																					<option value="TOMORROW">{translate('P2P_TIME_FRAME_TOMORROW', 'Tomorrow')}</option>
-																				</select>
-																			</div>
-																		</div>
-																		<div className="col-sm-6">
-																			<div className="form-group">
-																				<select className="form-control"
-																								value={this.props.timeFrameTime}
-																								onChange={this.props.timeFrameTimeChange}>
-																					<option value="6">12:00</option>
-																					<option value="7">13:00</option>
-																					<option value="8">14:00</option>
-																					<option value="9">15:00</option>
-																					<option value="10">16:00</option>
-																					<option value="11">17:00</option>
-																					<option value="12">12:00</option>
-																					<option value="13">13:00</option>
-																					<option value="14">14:00</option>
-																					<option value="15">15:00</option>
-																					<option value="16">16:00</option>
-																					<option value="17">17:00</option>
-																				</select>
-																			</div>
-																		</div>
-																	</div>
-																)
-															}
-														})()}
-
-														{(() =>{
-															if(payAccountId == 0){
-																return <Register />
-															}
-														})()}
-
-														{(() =>{
-															if(payAccountId != 0){
-																return <AmountController setAmount={setAmount} amount={amount}
-																												 limitsCheck={limitsCheck}/>
-															}
-														})()}
-
-														{(() =>{
-															if(isWithDraw && payAccountId != 0){
-																return <FeeController feeCashValue={feeCashValue} feeCheck={feeCheck} amount={amount}/>;
-															}
-														})()}
-
-													</div>
-												</div>
-												<p>Good news! You have a <span>100%</span> deposit bonus up to <span>$1,000.</span></p>
-											</div>
-										</div>
+						<div className="title">{processingTitle}</div>
+						<div className="infoCol">
+							<div className="row">
+								<div className="col-sm-3">
+									<div className="method active pull-left">
+										<img className="img-responsive" src={originPath + '/images/processors/16.png'}
+												 title={displayName}/>
 									</div>
+								</div>
+								<div className="col-sm-9">
+									<div className="form-group" id="payAccount">
+										<label className="control-label">{selectType}:</label>
+										{(() =>{
+											if(payAccountId != 0){
+												return (
+													<div id="selectPayAccount">
+															<SelectPayAccount setAmount={setAmount} amount={amount}/>
+															<button type='button' onClick={this.disablePayAccount} className='btn btn-xs btn-green'>
+																{deleteButton}
+															</button>
+													</div>
+												)
+											} else{
+												return (
+													<div id="payAccounts">
+														<SelectPayAccount setAmount={setAmount} amount={amount}/>
+													</div>
+												)
+											}
+										})()}
+
+										{(() =>{
+											if(payAccountId == 0){
+												return <Register />
+											}
+										})()}
+									</div>
+									<div className="form-group">
+										{(() =>{
+											if(payAccountId != 0 && !isWithDraw){
+												return (
+													<div id="timeFrame">
+														<label
+															className="control-label">{translate('P2P_TIME_FRAME', 'What time will you send these funds?')}</label>
+														<div className="col-sm-6">
+															<div className="form-group">
+																<select className="form-control"
+																				value={this.props.timeFrameDay}
+																				onChange={this.props.timeFrameDayChange}>
+																	<option value="TODAY">{translate('P2P_TIME_FRAME_TODAY', 'Today')}</option>
+																	<option value="TOMORROW">{translate('P2P_TIME_FRAME_TOMORROW', 'Tomorrow')}</option>
+																</select>
+															</div>
+														</div>
+														<div className="col-sm-6">
+															<div className="form-group">
+																<select className="form-control"
+																				value={this.props.timeFrameTime}
+																				onChange={this.props.timeFrameTimeChange}>
+																	<option value="6">12:00</option>
+																	<option value="7">13:00</option>
+																	<option value="8">14:00</option>
+																	<option value="9">15:00</option>
+																	<option value="10">16:00</option>
+																	<option value="11">17:00</option>
+																	<option value="12">12:00</option>
+																	<option value="13">13:00</option>
+																	<option value="14">14:00</option>
+																	<option value="15">15:00</option>
+																	<option value="16">16:00</option>
+																	<option value="17">17:00</option>
+																</select>
+															</div>
+														</div>
+													</div>
+												)
+											}
+										})()}
+
+										{(() =>{
+											if(payAccountId != 0){
+												return <AmountController setAmount={setAmount} amount={amount}
+																								 limitsCheck={limitsCheck}/>
+											}
+										})()}
+
+										{(() =>{
+											if(isWithDraw && payAccountId != 0){
+												return <FeeController feeCashValue={feeCashValue} feeCheck={feeCheck} amount={amount}/>;
+											}
+										})()}
+									</div>
+									<p><em>Good news! You have a <span>100%</span> deposit bonus up to <span>$1,000.</span></em></p>
 								</div>
 							</div>
 						</div>
