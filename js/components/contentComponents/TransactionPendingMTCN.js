63dc4dfb110c9d9d0e6c03f496a394b7c617aef0
joshgabry <josua.obando@gmail.com>
2016-08-09 17:48:17 -0600

    pending control number - in process

--- js/components/contentComponents/TransactionPendingMTCN.js
+++ js/components/contentComponents/TransactionPendingMTCN.js
@@ -10,33 +10,20 @@ let TransactionPendingMTCN = React.createClass({
 	render() {
 		let transactions = this.props.transactions;
 		return (
-			<div className=" table-responsive" id="transactionHistoryTable">
-				<table className="table table-striped">
-					<tbody>
-					<tr>
-						<th>Column</th>
-					</tr>
-					{(() =>{
-						if(transactions){
-							var rows = [];
-							transactions.map((transaction, i)=>{
-								rows.push(
-										<tr key={i}>
-									</tr>);
-							});
-							return rows;
-						} else{
-							return (
-								<tr>
-									<td colSpan="6">
-										No records!
-									</td>
-								</tr>
-							)
-						}
-					})()}
-					</tbody>
-				</table>
+			<div>
+				{(() =>{
+					if(transactions){
+						var tables = [];
+						transactions.map((transaction, i)=>{
+							tables.push(
+								<p key={i}>{transaction.Name}</p>
+							);
+						});
+						return tables;
+					} else{
+						return (<p>No records!</p>)
+					}
+				})()}
 			</div>
 		)
 	}
