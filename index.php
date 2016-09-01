ae96579ca542f6c390f4786472310f1a27a5d37e
jocampo <jocampoz@gmail.com>
2016-08-31 18:59:10 -0600

    rutas absolutas

--- index.php
+++ index.php
@@ -3,21 +3,21 @@
 <head>
 	<meta charset="utf-8" />
 	<title>Cashier</title>
-	<link rel="stylesheet" href="./css/bootstrap.min.css">
-	<link rel="stylesheet" href="./css/bootstrap-theme.min.css">
-	<link rel="stylesheet" href="./css/font-awesome.min.css">
-	<link rel="stylesheet" href="./css/style.css">
+	<link rel="stylesheet" href="/css/bootstrap.min.css">
+	<link rel="stylesheet" href="/css/bootstrap-theme.min.css">
+	<link rel="stylesheet" href="/css/font-awesome.min.css">
+	<link rel="stylesheet" href="/css/style.css">
 </head>
 <body>
 	<div id="app"></div>
 	<script>
   	  let loginInfo = <?php echo((count($_REQUEST)) > 0 ? json_encode($_REQUEST) : "{}") ?>;
   	</script>
-	<script src="./js/libs/jquery.min.js"></script>
-    <script src="./js/libs/stomp.js"></script>
-	<script src="./build/bundle.js"></script>
+	<script src="/js/libs/jquery.min.js"></script>
+    <script src="/js/libs/stomp.js"></script>
+	<script src="/build/bundle.js"></script>
 	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 	<!-- Include all compiled plugins (below), or include individual files as needed -->
-	<script src="./js/libs/bootstrap.min.js"></script>
+	<script src="/js/libs/bootstrap.min.js"></script>
 </body>
 </html>
