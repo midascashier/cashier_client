34fc9bf755e80cb25ae0c51a690782463aebde8e
jocampo <jocampoz@gmail.com>
2016-12-14 12:00:36 -0600

    changes

--- js/libs/chat.js
+++ js/libs/chat.js
@@ -1,4 +1,4 @@
 // English Version, also applied for every other language than spanish
 function chat(){
-	window.open('https://server.iad.liveperson.net/hc/58581018/?cmd=file&amp;file=visitorWantsToChat&amp;offlineURL=http://www.bookmaker.eu/LivePerson/Contact.asp?origin=www.bookmaker.eu&amp;email=customerservice@bookmaker.eu&amp;site=58581018&amp;referrer='+document.location, 'Chat', '321', '473');
+	window.open('https://livepokersupport.com/visitor/index.php?/Default/LiveChat/Chat/Request/_sessionID=/_promptType=chat/_proactive=0/_filterDepartmentID=33%2C48/_randomNumber=oj6266h15mhaw6j436e0d6l3yyg2g6n2/_fullName=/_email=/_languageID=1', 'livechatwin', 'toolbar=0,location=0,directories=0,status=1,menubar=0,scrollbars=0,resizable=1,width=600,height=680');
 }
