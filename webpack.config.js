9bda4801d97e87ae7336de783148579175d98eb7
josuaobando <josua.obando@gmail.com>
2017-06-29 16:05:33 -0600

    Updated: generate version for live

--- webpack.config.js
+++ webpack.config.js
@@ -4,13 +4,13 @@ module.exports = {
 	entry: ['babel-polyfill', './js/main.jsx'],
 
 	plugins: [
-		new webpack.DefinePlugin({ 'process.env.NODE_ENV': "'production'" })
+		new webpack.DefinePlugin({'process.env.NODE_ENV': "'production'"})
 	],
 
 	output: {
 		// If in production mode we put the files into the dist folder instead
 		path: process.env.NODE_ENV === 'production' ? './dist/' : './build/',
-		filename: process.env.NODE_ENV === 'production' ? 'cashier_client-2473452404861.min.js' : 'bundle.js'
+		filename: process.env.NODE_ENV === 'production' ? 'cashier_client-' + new Date().getTime() + '.min.js' : 'bundle.js'
 	},
 
 	devtool: "source-map",
