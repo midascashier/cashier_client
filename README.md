b70ea9cea432b8b569193304cfa19dc90ad99795
josuaobando <josua.obando@gmail.com>
2017-08-09 10:48:57 -0600

    FIX: validation is added, when the customer does not have a registered debit card. Add FORM

--- README.md
+++ README.md
@@ -3,5 +3,5 @@
 At the terminal:
 
 - npm install to install the dependencies
-- npm sun build: to generate dev version (on "/build" folder)
+- npm run build: to generate dev version (on "/build" folder)
 - npm run deploy: to generate live version (copy min.js and .map on "/build" folder)
--- js/components/methods/debitcards/DebitCards.js
+++ js/components/methods/debitcards/DebitCards.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import { CashierStore } from '../../../stores/CashierStore'
-import { LoadingSpinner } from '../../../components/loading/LoadingSpinner'
-import { AskInfo } from './AskInfo'
-import { InfoMethod } from './InfoMethod'
-import { TransactionService } from '../../../services/TransactionService'
+import {CashierStore} from '../../../stores/CashierStore'
+import {LoadingSpinner} from '../../../components/loading/LoadingSpinner'
+import {AskInfo} from './AskInfo'
+import {InfoMethod} from './InfoMethod'
+import {TransactionService} from '../../../services/TransactionService'
 import cashier from '../../../constants/Cashier'
 
 let DebitCards = React.createClass({
@@ -71,44 +71,44 @@ let DebitCards = React.createClass({
 						{(() =>{
 							if(!this.state.selectedProcessor.processorId){
 								return <LoadingSpinner />;
-							} else{
+							}else{
 								return <InfoMethod amount={this.props.amount} limitsCheck={this.props.limitsCheck}/>;
 							}
 						})()}
 					</div>
 				</div>
 			)
-		} else{
-			if(this.state.payAccount.displayName === cashier.NO_PAYACCOUNTS){
-				return (
-					<div id="debitCards">
-						<div className="col-md-6 col-md-offset-3">
-							<div className="box">
-								<div className="title">DebitCards</div>
-								<div className="infoCol">
-									<p><a target="_blank" href="/docs/common/PREPAID_CARD_APPLICATION_FORM.pdf">
-										<strong>NEW TO DEBIT CARDS PLEASE CLICK HERE TO PRINT OUT THE KYC FORM</strong>
-									</a></p>
-									<p>This form will be required by customer service to proceed with the FREE delivery of your Debit
-										Card.</p>
-									<p>In order to apply for a prepaid debit, follow these easy steps:</p>
-									<p><span>1.</span> Scan or photograph one of the following options:</p>
-									<ul>
-										<li>Passport</li>
-										<li>National ID</li>
-										<li>Driver's license with SSC</li>
-									</ul>
-									<p><span>2.</span> Scan or photograph your utility bill (must match the address and the name on the KYC Form)</p>
-									<p><span>3.</span> Scan or photograph the KYC form</p>
-									<p><span>4.</span> Email your scanned documents to debitcards@bookmaker.eu</p>
-								</div>
+		}else if(!this.state.payAccount.displayName || this.state.payAccount.displayName === null || this.state.payAccount.displayName == cashier.NO_RESPONSE){
+
+			return (
+				<div id="debitCards">
+					<div className="col-md-6 col-md-offset-3">
+						<div className="box">
+							<div className="title">DebitCards</div>
+							<div className="infoCol">
+								<p><a target="_blank" href="/docs/common/PREPAID_CARD_APPLICATION_FORM.pdf">
+									<strong>NEW TO DEBIT CARDS PLEASE CLICK HERE TO PRINT OUT THE KYC FORM</strong>
+								</a></p>
+								<p>This form will be required by customer service to proceed with the FREE delivery of your Debit
+									Card.</p>
+								<p>In order to apply for a prepaid debit, follow these easy steps:</p>
+								<p><span>1.</span> Scan or photograph one of the following options:</p>
+								<ul>
+									<li>Passport</li>
+									<li>National ID</li>
+									<li>Driver's license with SSC</li>
+								</ul>
+								<p><span>2.</span> Scan or photograph your utility bill (must match the address and the name on the KYC Form)</p>
+								<p><span>3.</span> Scan or photograph the KYC form</p>
+								<p><span>4.</span> Email your scanned documents to debitcards@bookmaker.eu</p>
 							</div>
 						</div>
 					</div>
-				)
-			} else{
-				return <div className="col-sm-12"><LoadingSpinner /></div>;
-			}
+				</div>
+			)
+
+		}else{
+			return <div className="col-sm-12"><LoadingSpinner /></div>;
 		}
 	}
 });
