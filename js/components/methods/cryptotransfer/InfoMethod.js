15b7a38e0ab8b996db9d3534b686f0a1905d97d9
henmanuel <admin@loopzone.org>
2017-11-07 19:08:32 -0600

    Crypto Transfer

--- js/components/methods/cryptotransfer/InfoMethod.js
+++ js/components/methods/cryptotransfer/InfoMethod.js
@@ -119,7 +119,6 @@ let InfoMethod = React.createClass({
 				isNextDisabled = "";
 			}
 		}
-
 		let nextBTN = (
 			<button type='button' onClick={this.continueTransaction} disabled={isNextDisabled} className='btn btn-green'>
 				{translate('PROCESSING_BUTTON_NEXT', 'Next')}
