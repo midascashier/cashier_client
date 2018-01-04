5fba9918f60b65ad767b5b634867e23b6a917212
henmanuel <admin@loopzone.org>
2018-01-04 12:07:13 -0600

    Crypto exclude, FAQ  and currency name problems

--- js/components/headerComponents/CompanyInfo.js
+++ js/components/headerComponents/CompanyInfo.js
@@ -20,8 +20,8 @@ let CompanyInfo = React.createClass({
 	/**
 	 * function to open chat window
 	 */
-	openFAQ(event) {
-		FAQ();
+	openFAQ() {
+		window.open('https://den.secureprivate.com/FAQ.html', 'FAQ', 'toolbar=0,location=0,directories=0,status=1,menubar=0,scrollbars=1,resizable=0,width=800,height=680');
 	},
 
 	render() {
--- js/libs/general.js
+++ js/libs/general.js
@@ -3,10 +3,6 @@ function chat(){
 	window.open('https://livepokersupport.com/visitor/index.php?/Default/LiveChat/Chat/Request/_sessionID=/_promptType=chat/_proactive=0/_filterDepartmentID=33%2C48/_randomNumber=oj6266h15mhaw6j436e0d6l3yyg2g6n2/_fullName=/_email=/_languageID=1', 'livechatwin', 'toolbar=0,location=0,directories=0,status=1,menubar=0,scrollbars=0,resizable=1,width=600,height=680');
 }
 
-function FAQ(){
-	window.open('https://den.secureprivate.com/FAQ.html', 'FAQ', 'toolbar=0,location=0,directories=0,status=1,menubar=0,scrollbars=1,resizable=0,width=800,height=680');
-}
-
 function capitalize(string,a) {
 	var tempstr = string.toLowerCase();
 	if (a == false || a == undefined)
@@ -14,8 +10,4 @@ function capitalize(string,a) {
 	else {
 		return tempstr.split(" ").map(function (i) { return i[0].toUpperCase() + i.substring(1) }).join(" ");
 	}
-}
-
-function debitCardsForm(){
-	window.open('https://de.secureprivate.com/docs/common/PREPAID_CARD_APPLICATION_FORM.pdf', 'form', 'toolbar=0,location=0,directories=0,status=1,menubar=0,scrollbars=0,resizable=1,width=600,height=680');
 }
