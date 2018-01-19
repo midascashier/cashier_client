9b2eb343063282476c17a604afe94f93d6f4179a
henmanuel <admin@loopzone.org>
2018-01-19 12:03:22 -0600

    Crypto Transfer 2.0

---
+++ js/components/methods/cryptotransfer/ConfirmWithdraw.js
@@ -0,0 +1,166 @@
+import React from 'react'
+import { UIService } from '../../../services/UIService'
+import { translate } from '../../../constants/Translate'
+import { CashierStore } from '../../../stores/CashierStore'
+import { TransactionService } from '../../../services/TransactionService'
+
+let CryptoTransferConfirmWithdraw = React.createClass({
+
+	/**
+	 * React function to set component initial state
+	 */
+	getInitialState(){
+		return this.refreshLocalState();
+	},
+
+	/**
+	 * this function sets and return object with local states
+	 */
+	refreshLocalState() {
+		return {
+			transaction: CashierStore.getTransaction(),
+			payAccount: CashierStore.getCurrentPayAccount()
+		}
+	},
+
+	/**
+	 * this is the callback function the store calls when a state change
+	 *
+	 * @private
+	 */
+	_onChange() {
+		this.setState(this.refreshLocalState());
+	},
+
+	/**
+	 * this function sends transaction info to cashier
+	 *
+	 */
+	processTransaction(){
+		let transaction = this.state.transaction;
+
+		let dynamicParams = {
+			amount: transaction.amount,
+			payAccountId: transaction.payAccountId,
+			promoCode: transaction.promoCode,
+			cryptoAddress: transaction.cryptoAddress,
+			currencyName: transaction.currencyName,
+			currencySymbol: transaction.currencySymbol,
+			BTCConversionAmount: transaction.BTCConversionAmount
+		};
+
+		TransactionService.processCryptoTransfer(dynamicParams, 'ticket');
+	},
+
+	/**
+	 * Sends customer to previous step and start transaction
+	 */
+	editWithdraw(){
+		UIService.startTransaction();
+	},
+
+	/**
+	 * send the customer to select the processor again
+	 */
+	setFirstStep() {
+		UIService.setFirstStep();
+	},
+
+	render(){
+		let originPath = UIService.getOriginPath();
+		let transaction = this.state.transaction;
+		let limits = UIService.getProcessorLimitMinMax();
+		return (
+			<div id="confirmBitCoinWithdraw" className="internal-content">
+				<div className="row">
+					<div className="col-sm-12">
+						<div className="modules">
+							<div className="row">
+
+								<div className="col-sm-6 ">
+									<div className="box">
+
+										<div className="row">
+											<div className="col-sm-12">
+												<div
+													className="title">{translate('PROCESSING_BILLING_INFO_TITLE', 'Double-check Your Billing Information')}</div>
+												<div className="infoCol text-justify">
+													<p>
+														Crypto Transfer withdraws will be process inside 24 hours, but are typically processed within an
+														hour.
+													</p>
+												</div>
+											</div>
+
+										</div>
+									</div>
+								</div>
+
+								<div className="col-sm-6">
+									<div className="box">
+
+										<div className="row">
+											<div className="col-sm-12">
+												<div className="title">{translate('METHOD_DETAILS_WITHDRAW', 'Withdraw Details')}</div>
+												<div className="table-responsive">
+													<table className="table table-striped">
+														<tbody>
+															<tr>
+																<td>{translate('TRANSACTION_AMOUNT', 'Amount')}</td>
+																<td><span>{transaction.amount + ' ' + limits.currencyCode}</span></td>
+															</tr>
+															<tr>
+																<td>{translate('CRYPTO_DEPOSIT_ADDRESS', 'Address')}</td>
+																<td><span>{transaction.cryptoAddress}</span></td>
+															</tr>
+														</tbody>
+													</table>
+												</div>
+												<p>
+													<i className="fa fa-pencil green"></i>
+													<a onClick={this.editWithdraw}>{translate('METHOD_EDIT_DETAILS_WITHDRAW', 'Edit the withdraw details')}</a>
+												</p>
+												<div className="row">
+													<div className="col-sm-6">
+														<button type="button" onClick={this.processTransaction} className="btn btn-green">
+															{translate('PROCESSING_BUTTON_COMPLETE_WITHDRAW', 'Complete Withdraw')}
+														</button>
+														<p>
+															<a onClick={this.setFirstStep}>{translate('METHOD_USE_DIFFERENT', 'Use a different method.')}</a>
+														</p>
+													</div>
+													<div className="col-sm-6">
+														<img src={originPath + '/images/ssl.png'} alt="ssl"/>
+													</div>
+												</div>
+											</div>
+										</div>
+
+									</div>
+								</div>
+
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		)
+	},
+
+	/**
+	 * React function to add listener to this component once is mounted
+	 * here the component listen changes from the store
+	 */
+	componentDidMount() {
+		CashierStore.addChangeListener(this._onChange);
+	},
+
+	/**
+	 * React function to remove listener to this component once is unmounted
+	 */
+	componentWillUnmount() {
+		CashierStore.removeChangeListener(this._onChange);
+	}
+});
+
+module.exports.CryptoTransferConfirmWithdraw = CryptoTransferConfirmWithdraw;
---
+++ js/components/methods/cryptotransfer/tickets/PendingTicket.js
@@ -0,0 +1,173 @@
+import React from 'react'
+import QRCode from 'qrcode.react'
+import { UIService } from '../../../../services/UIService'
+import { CashierStore } from '../../../../stores/CashierStore'
+import { translate } from '../../../../constants/Translate'
+import { TransactionService } from '../../../../services/TransactionService'
+
+let CryptoTransferTicketPending = React.createClass({
+
+	/**
+	 * initialize the state
+	 *
+	 * @returns {*|{address, amount, minutes}|{address: string, amount: string, minutes: number}}
+	 */
+	getInitialState(){
+		return Object.assign(this.refreshLocalState());
+	},
+
+	/**
+	 * build the state
+	 *
+	 * @returns {{address: string, amount: string, minutes: number}}
+	 */
+	refreshLocalState() {
+		let address = "Loading...";
+		let cryptoName = "Loading...";
+		let cryptoAmount = "Loading...";
+		let CryptoCurrencyISO = "Loading...";
+
+		let transaction = UIService.getLastTransactionResponse();
+		TransactionService.cryptoTransferTransaction(transaction.transactionId);
+
+		if(transaction && transaction.details && transaction.details.cryptoTransferTransaction){
+			address = transaction.details.cryptoTransferTransaction.AddressFrom;
+			cryptoName = transaction.details.cryptoTransferTransaction.CryptoCurrencyName;
+			cryptoAmount = transaction.details.cryptoTransferTransaction.CryptoCurrencyAmount;
+			CryptoCurrencyISO = transaction.details.cryptoTransferTransaction.CryptoCurrencyISO;
+		}
+
+		return {
+			address : address,
+			cryptoName : cryptoName,
+			cryptoAmount : cryptoAmount,
+			CryptoCurrencyISO : CryptoCurrencyISO
+		}
+	},
+
+	/**
+	 * refresh the state when changes occur
+	 *
+	 * @private
+	 */
+	_onChange() {
+		this.setState(this.refreshLocalState());
+	},
+
+	/**
+	 * copy to clipboard the Crypto Address Address
+	 */
+	copyToClipboard() {
+		let address = this.state.address;
+		let clipBoard = document.createElement("input");
+		clipBoard.setAttribute("value", address);
+		document.body.appendChild(clipBoard);
+		clipBoard.select();
+		document.execCommand("copy");
+		document.body.removeChild(clipBoard);
+	},
+
+	render() {
+
+		let address = this.state.address;
+		let cryptoName = this.state.cryptoName;
+		let cryptoAmount = this.state.cryptoAmount;
+		let amount = translate('CRYPTO_INSTRUCTIONS_AMOUNT', 'Send crypto Amount from your wallet', { cryptoAmount: cryptoAmount, cryptoCurrency : cryptoName });
+		let cryptoInstructions = translate('CRYPTO_INSTRUCTIONS', 'Now send your crypto currency to us.', { cryptoCurrency : cryptoName });
+		let addressInstructions = translate('CRYPTO_INSTRUCTIONS_ADDRESS', 'Send the crypto currency to the following address', { cryptoCurrency : cryptoName });
+		let addressInfoInstructions = translate('CRYPTO_INSTRUCTIONS_ADDRESS_INFO', 'Please include any Miners Fee your crypto currency wallet charges.', { cryptoCurrency : cryptoName });
+
+		return (
+			<div id="CryptoAddressTicketInstructions">
+
+				<div className="col-sm-12">
+					<div className="rejected-message">
+						<div className="title">{cryptoInstructions}</div>
+					</div>
+				</div>
+
+				<div className="col-sm-12">
+					<div className="modules">
+						<div className="row">
+
+							<div className="col-sm-5">
+								<div className="box">
+									<div className="row">
+										<div className="col-sm-12">
+											<div className="title">#1</div>
+											<div className="infoCol">
+												<div className="subtitle">{amount}</div>
+												<p><strong>{translate('IMPORTANT')}</strong>{translate('WARNING_MINER_DEPOSIT')}<strong>{this.state.cryptoAmount} {cryptoName}</strong>,{translate('WARNING_MINER_DEPOSIT_END')}</p>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+
+							<div className="col-sm-5">
+								<div className="box">
+									<div className="row">
+										<div className="col-sm-12">
+											<div className="row">
+												<div className="col-sm-12">
+													<div className="title">#2</div>
+													<div className="infoCol">
+														<div className="subtitle">{addressInstructions}</div>
+														<p>{addressInfoInstructions}</p>
+
+														<div className="row">
+															<div id="cryptoAddress" className="form-group">
+																<div className="col-sm-12">
+																	<input type="text" className="form-control" id="cryptoAddress" value={address} readOnly/>
+																</div>
+																<div className="col-sm-12 mod-center">
+																	<button type='button' onClick={this.copyToClipboard} disabled={!address} className='btn btn-green'>
+																		{translate('PROCESSING_BUTTON_COPY', 'Copy')}
+																	</button>
+																</div>
+															</div>
+														</div>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+
+							<div className="col-sm-2">
+								<div id="QRCode">
+									{(() =>{
+										if(address){
+											return (
+												<div className="mod-center img-responsive center-block">
+													<QRCode value={address}/>
+												</div>
+											)
+										}
+									})()}
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		)
+	},
+
+	/**
+	 * component is ready
+	 */
+	componentDidMount() {
+		CashierStore.addChangeListener(this._onChange);
+	},
+
+	/**
+	 * React function to remove listener to this component once is unmounted
+	 */
+	componentWillUnmount() {
+		CashierStore.removeChangeListener(this._onChange);
+	}
+});
+
+module.exports.CryptoTransferTicketPending = CryptoTransferTicketPending;
