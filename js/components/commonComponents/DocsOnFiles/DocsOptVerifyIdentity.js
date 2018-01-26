365341445205ccb52067047c747c06cc48d83e44
henmanuel <admin@loopzone.org>
2018-01-26 09:58:31 -0600

    Agregar "DocsOnFile" al Nuevo Poker

--- js/components/commonComponents/DocsOnFiles/DocsOptVerifyIdentity.js
+++ js/components/commonComponents/DocsOnFiles/DocsOptVerifyIdentity.js
@@ -19,6 +19,14 @@ let DocsOptVerifyIdentity = React.createClass({
                     <span className="switch"/>
                     <span>{translate('MY_REQUEST_DOCS_OPTION_VE_EW_TXT')}</span>
                 </div>
+
+                <div id="DrawDropUploadContent">
+                    <form id="DrawDropUpload">
+                        <input type="file" multiple/>
+                        <p>Drag your files here or click in this area.</p>
+                        <button type="submit">Upload</button>
+                    </form>
+                </div>
             </div>
         )
     }
