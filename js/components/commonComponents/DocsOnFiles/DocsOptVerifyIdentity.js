11b92bbff5b1ded4106ee4ff546dd3670ae8f75e
henmanuel <admin@loopzone.org>
2018-01-30 10:17:26 -0600

    Agregar "DocsOnFile" al Nuevo Poker

--- js/components/commonComponents/DocsOnFiles/DocsOptVerifyIdentity.js
+++ js/components/commonComponents/DocsOnFiles/DocsOptVerifyIdentity.js
@@ -3,6 +3,23 @@ import {DrawDropUpload} from '../files/DrawDropUpload'
 import {translate} from '../../../constants/Translate'
 
 let DocsOptVerifyIdentity = React.createClass({
+
+    getInitialState(){
+        return{check: false}
+    },
+
+    switchVerifyType(e){
+        let check = e.target.checked;
+
+        this.setState({
+            check: check
+        });
+    },
+
+    verifyIdOptionsChange(){
+        let activoFijo = $('input[name="activoFijo"]:checked').val();
+    },
+
     render(){
         return(
             <div id="CheckIdContent">
@@ -10,7 +27,7 @@ let DocsOptVerifyIdentity = React.createClass({
 
                 <div id="switchOpt">
                     <label className="switch">
-                        <input type="checkbox"/>
+                        <input type="checkbox" onChange={this.switchVerifyType}/>
                         <span className="slider round"/>
                     </label>
                 </div>
@@ -21,7 +38,38 @@ let DocsOptVerifyIdentity = React.createClass({
                     <span>{translate('MY_REQUEST_DOCS_OPTION_VE_EW_TXT')}</span>
                 </div>
 
-                <DrawDropUpload/>
+                {(() =>{
+                    if(!this.state.check) {
+                        return (
+                            <div id="CheckIdVerifyOptions">
+                                <input id="driverLicense" type="radio" name="verifyOptionsType" className="input-hidden" value="1" onChange={this.verifyIdOptionsChange}/>
+                                <label for="driverLicense">
+                                    <img src="../images/driverLicenseOption.png" alt="I'm sad"/>
+                                </label>
+
+                                <input id="idDocument" type="radio" name="verifyOptionsType" className="input-hidden" value="2" onChange={this.verifyIdOptionsChange}/>
+                                <label for="idDocument">
+                                    <img src="../images/idDocumentOption.png" alt="I'm happy"/>
+                                </label>
+
+                                <input id="passport" type="radio" name="verifyOptionsType" className="input-hidden" value="3" onChange={this.verifyIdOptionsChange}/>
+                                <label for="passport">
+                                    <img src="../images/passportOption.png" alt="I'm happy"/>
+                                </label>
+                            </div>
+                        )
+                    }
+                })()}
+
+                {(() =>{
+                    if(!this.state.check || (this.state.check)) {
+                        return(
+                            <div id="DrawDropUploadContent">
+                                <DrawDropUpload/>
+                            </div>
+                        )
+                    }
+                })()}
             </div>
         )
     }
