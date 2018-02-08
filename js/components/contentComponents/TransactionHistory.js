3c751cd1d1d8cdbe41299a129a2a0883a0bda9f7
josuaobando <Josh2909>
2018-02-08 13:22:45 -0600

    - change status tag

--- js/components/contentComponents/TransactionHistory.js
+++ js/components/contentComponents/TransactionHistory.js
@@ -50,89 +50,92 @@ let TransactionHistory = React.createClass({
 				<div className="table-responsive" id="transactionHistoryTable">
 					<table className="table table-striped">
 						<tbody>
-							<tr>
-								<th>{translate('TRANSACTION_HISTORY_TABLE_COL_DATE', 'Date')}</th>
-								<th>{translate('TRANSACTION_HISTORY_TABLE_COL_TYPE', 'Type')}</th>
-								<th>{translate('TRANSACTION_HISTORY_TABLE_COL_METHOD', 'Method')}</th>
-								<th>{translate('TRANSACTION_HISTORY_TABLE_COL_AMOUNT', 'Amount')}</th>
-								<th>{translate('TRANSACTION_HISTORY_TABLE_COL_STATUS', 'Status')}</th>
-								<th></th>
-							</tr>
-							{(() =>{
-								if(transactions){
-									let rows = [];
-									transactions.map((transaction, i) =>{
-										let status = transaction.TransactionStatus.toLowerCase();
-										let fontColor = "black";
-										switch(status){
-											case "failed":
-												fontColor = "tomato";
+						<tr>
+							<th>{translate('TRANSACTION_HISTORY_TABLE_COL_DATE', 'Date')}</th>
+							<th>{translate('TRANSACTION_HISTORY_TABLE_COL_TYPE', 'Type')}</th>
+							<th>{translate('TRANSACTION_HISTORY_TABLE_COL_METHOD', 'Method')}</th>
+							<th>{translate('TRANSACTION_HISTORY_TABLE_COL_AMOUNT', 'Amount')}</th>
+							<th>{translate('TRANSACTION_HISTORY_TABLE_COL_STATUS', 'Status')}</th>
+							<th></th>
+						</tr>
+						{(() =>{
+							if(transactions){
+								let rows = [];
+								transactions.map((transaction, i) =>{
+									let status = transaction.TransactionStatus.toLowerCase();
+									let fontColor = "black";
+									switch(status){
+										case "failed":
+										case "deferred":
+											fontColor = "purple";
 											break;
 
-											case "deferred":
-												fontColor = "purple";
+										case "processing":
+											fontColor = "blue";
 											break;
 
-											case "processing":
-												fontColor = "blue";
+										case "rejected":
+											fontColor = "red";
 											break;
 
-											case "rejected":
-												fontColor = "red";
+										case "pre-approved":
+											fontColor = "aqua";
 											break;
 
-											case "pre-approved":
-												fontColor = "aqua";
+										case "cancelled":
+											fontColor = "navy";
 											break;
 
-											case "cancelled":
-												fontColor = "navy";
+										case "approved":
+											fontColor = "green";
 											break;
 
-											case "approved":
-												fontColor = "green";
+										case "pending":
+											fontColor = "orange";
 											break;
+									}
 
-											case "pending":
-												fontColor = "orange";
-											break;
-										}
+									let cancelButton = "";
 
-										let cancelButton = "";
-
-										if(transaction.caTransactionType_Id == cashier.TRANSACTION_TYPE_PAYOUT && (transaction.caTransactionStatus_Id == cashier.TRANSACTION_STATUS_DEFERRED || transaction.caTransactionStatus_Id == cashier.TRANSACTION_STATUS_PRE_APPROVE)){
-											pendingPayouts.map((payout) =>{
-												if (transaction.caJournal_Id == payout['caJournal_Id'] && cancelPayoutButton){
-													cancelButton =	<button type="button" id={transaction.caJournal_Id} className = "btn btn-grey"
-														onClick={this.cancelPendingPayout.bind(this, transaction.caJournal_Id)}
-														class="btn btn-grey">
+									if(transaction.caTransactionType_Id == cashier.TRANSACTION_TYPE_PAYOUT && (transaction.caTransactionStatus_Id == cashier.TRANSACTION_STATUS_DEFERRED || transaction.caTransactionStatus_Id == cashier.TRANSACTION_STATUS_PRE_APPROVE)){
+										pendingPayouts.map((payout) =>{
+											if(transaction.caJournal_Id == payout['caJournal_Id'] && cancelPayoutButton){
+												cancelButton = <button type="button" id={transaction.caJournal_Id} className="btn btn-grey"
+																							 onClick={this.cancelPendingPayout.bind(this, transaction.caJournal_Id)}
+																							 class="btn btn-grey">
 													Cancel</button>;
-												}
-											});
-										}
+											}
+										});
+									}
 
-										rows.push(
-											<tr key={i} className={transaction.TransactionStatus.toLowerCase()}>
-												<td>{transaction.DateTrans}</td>
-												<td>{translate('TRANSACTION_TYPE_ID_' + transaction.caTransactionType_Id, transaction.TransactionType)}</td>
-												<td>{transaction.Method}</td>
-												<td>{ApplicationService.currency_format(transaction.CurrencyAmount) + ' ' + transaction.CurrencyCode}</td>
-												<td className={status}>
-													<span className={fontColor}>{translate('TRANSACTION_STATUS_' + transaction.TransactionStatus.toUpperCase(), transaction.TransactionStatus)}</span>
-												</td>
-												<td>{cancelButton}</td>
-											</tr>
-										);
-									});
-									return rows;
-								}else{
-									return (
-										<tr>
-											<td colSpan="6">No records!</td>
+									rows.push(
+										<tr key={i} className={transaction.TransactionStatus.toLowerCase()}>
+											<td>{transaction.DateTrans}</td>
+											<td>{translate('TRANSACTION_TYPE_ID_' + transaction.caTransactionType_Id, transaction.TransactionType)}</td>
+											<td>{transaction.Method}</td>
+											<td>{ApplicationService.currency_format(transaction.CurrencyAmount) + ' ' + transaction.CurrencyCode}</td>
+											<td className={status}>
+												{(() =>{
+													if(transaction.TransactionStatus.toUpperCase() == 'DEFERRED'){
+														return <span className={fontColor}>{translate('TRANSACTION_STATUS_PENDING', 'Penging')}</span>
+													}else{
+														return <span className={fontColor}>{translate('TRANSACTION_STATUS_' + transaction.TransactionStatus.toUpperCase(), transaction.TransactionStatus)}</span>
+													}
+												})()}
+											</td>
+											<td>{cancelButton}</td>
 										</tr>
-									)
-								}
-							})()}
+									);
+								});
+								return rows;
+							}else{
+								return (
+									<tr>
+										<td colSpan="6">No records!</td>
+									</tr>
+								)
+							}
+						})()}
 						</tbody>
 					</table>
 				</div>
