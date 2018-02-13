67bc8991649d68268d1243f23470a81361411b39
henmanuel <admin@loopzone.org>
2018-02-13 17:39:49 -0600

    Agregar "DocsOnFile" al Nuevo Poker

--- js/components/commonComponents/DocsOnFiles/DocsUploadSuccessResponse.js
+++ js/components/commonComponents/DocsOnFiles/DocsUploadSuccessResponse.js
@@ -2,11 +2,25 @@ import React from 'react'
 import {translate} from '../../../constants/Translate'
 
 let DocsUploadSuccessResponse = React.createClass({
+    propsType : {
+        responseType : React.PropTypes.bool
+    },
+
     render(){
+        if(this.props.responseType){
+            return(
+                <div id="DocsUploadSuccessResponse">
+                    <img src="/images/check1.svg"/>
+                    <span>{translate('DOCS_FILE_UPLOAD_SUCCESS_RESPONSE')}</span>
+                    <p>{translate('DOCS_FILE_UPLOAD_SUCCESS_NOTIFIED_')}</p>
+                </div>
+            )
+        }
+
         return(
             <div id="DocsUploadSuccessResponse">
-                <img src="/images/check1.svg"/>
-                <span>{translate('DOCS_FILE_UPLOAD_SUCCESS_RESPONSE')}</span>
+                <img src="/images/check2.svg"/>
+                <span>{translate('DOCS_FILE_UPLOAD_ERROR_RESPONSE')}</span>
                 <p>{translate('DOCS_FILE_UPLOAD_SUCCESS_NOTIFIED_')}</p>
             </div>
         )
