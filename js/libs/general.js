d746e7693bc21d627a24d61da8f799f4014775b8
henmanuel <admin@loopzone.org>
2018-02-14 11:06:59 -0600

    Agregar "DocsOnFile" al Nuevo Poker

--- js/libs/general.js
+++ js/libs/general.js
@@ -3,8 +3,8 @@ function chat(){
 	window.open('https://livepokersupport.com/visitor/index.php?/Default/LiveChat/Chat/Request/_sessionID=/_promptType=chat/_proactive=0/_filterDepartmentID=33%2C48/_randomNumber=oj6266h15mhaw6j436e0d6l3yyg2g6n2/_fullName=/_email=/_languageID=1', 'livechatwin', 'toolbar=0,location=0,directories=0,status=1,menubar=0,scrollbars=0,resizable=1,width=600,height=680');
 }
 
-function capitalize(string,a) {
-	var tempstr = string.toLowerCase();
+function capitalize(string,a){
+	var tempstr = string;
 	if (a == false || a == undefined)
 		return tempstr.replace(tempstr[0], tempstr[0].toUpperCase());
 	else {
