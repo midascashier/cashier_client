932ff7de90c65d65f682e0387a1a083be2e9ecde
josuaobando <Josh2909>
2018-10-04 16:58:19 -0600

    rollback live support

--- js/libs/general.js
+++ js/libs/general.js
@@ -1,6 +1,8 @@
 // English Version, also applied for every other language than spanish
 function chat(){
-	$("img[class*='LPMimage']").trigger('click');
+	//Live Person
+	//$("img[class*='LPMimage']").trigger('click');
+	window.open('https://lz.livepokersupport.com/chat.php?v=2&hcgs=MQ__&nct=MQ__&hfk=MQ__','','width=400,height=600,left=0,top=0,resizable=yes,menubar=no,location=no,status=yes,scrollbars=yes');
 }
 
 function capitalize(string, a){
