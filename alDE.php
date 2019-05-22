b4776d4fb24b6571e6c11f14708bfcc468fe271f
TavoGM <tavogm@gmail.com>
2019-05-22 16:46:16 -0600

    temporary http referrer

--- alDE.php
+++ alDE.php
@@ -63,6 +63,7 @@
       <input type="hidden" id="username" name="username" value=<?php echo $_REQUEST["username"] ?>>
       <input type="hidden" id="password" name="password" value=<?php echo $_REQUEST["password"] ?>>
       <input type="hidden" id="option" name="option" value=<?php echo $_REQUEST["option"] ?>>
+      <input type="hidden" id="referrer" name="referrer" value=<?php echo $_SERVER['HTTP_REFERER'] ?>>
       <input type="hidden" id="ioBB" name="ioBB" value="">
       <input type="hidden" id="atDeviceId" name="atDeviceId" value="">
       <input type="hidden" id="f" name="doLogin" value="1">
