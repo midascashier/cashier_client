8d6d94c807194c742777d848a4ba202de78ce908
josuaobando <Josh2909>
2019-06-11 14:04:55 -0600

    Add validation for pending status without redirection

--- js/components/methods/skrill/tickets/PendingTicket.js
+++ js/components/methods/skrill/tickets/PendingTicket.js
@@ -1,14 +1,24 @@
 import React from 'react'
+import {CashierStore} from '../../../../stores/CashierStore'
+import {PendingTicket} from '../../../methodTickets/PendingTicket'
 import {RedirectTicket} from '../../../methodTickets/RedirectTicket'
 
 let SkrillTicketPending = React.createClass({
-
 	render(){
-		return (
-			<div>
-				<RedirectTicket/>
-			</div>
-		)
+		let transaction = CashierStore.getLastDataTransactionResponse();
+		if("gotoURLAction" in transaction && "gotoURL" in transaction){
+			return (
+				<div>
+					<RedirectTicket/>
+				</div>
+			)
+		}else{
+			return (
+				<div>
+					<PendingTicket/>
+				</div>
+			)
+		}
 	}
 });
 
