3c5f8624d785d619c5babb7663692b781e5a4282
Alonso Flores <arks20flame@hotmail.com>
2019-07-04 16:19:34 -0600

    Implemented a condition for Visa Pending tickets. -gotoURLAction for RedirectTicket. -show the PendingTicket in other cases.

--- js/components/methods/visa/tickets/PendingTicket.js
+++ js/components/methods/visa/tickets/PendingTicket.js
@@ -1,15 +1,26 @@
 import React from 'react'
+import {CashierStore} from '../../../../stores/CashierStore'
 import {RedirectTicket} from '../../../methodTickets/RedirectTicket'
+import {PendingTicket} from '../../../methodTickets/PendingTicket'
 
 let VisaPendingTicket = React.createClass({
 
-	render(){
-		return (
-			<div>
-				<RedirectTicket/>
-			</div>
-		)
-	}
+  render(){
+    let transaction = CashierStore.getLastDataTransactionResponse();
+    if("gotoURLAction" in transaction && "gotoURL" in transaction){
+      return (
+          <div>
+            <RedirectTicket/>
+          </div>
+      )
+    }else{
+      return (
+          <div>
+            <PendingTicket/>
+          </div>
+      )
+    }
+  }
 });
 
 module.exports.VisaPendingTicket = VisaPendingTicket;
